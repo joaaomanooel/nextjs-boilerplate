70cab057a43bfe6c7d88fcc20ff8dbbd0b539b23
João Manoel Neto <joaaomanooel@gmail.com>
2020-08-21 00:49:35 -0300

    move source files to "src" path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = 'React Avançado' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Advantege React</a>
         </h1>
 
         <p className={styles.description}>
@@ -61,5 +65,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
